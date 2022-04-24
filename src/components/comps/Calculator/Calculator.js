43c9155f99c369a7ee638a52cd238e3f483ec14c
kfir9 <kfir950@gmail.com>
2022-04-24 22:42:15 +0300

    Basic Calculator Design

---
+++ src/components/comps/Calculator/Calculator.js
@@ -0,0 +1,21 @@
+import {Component} from "react";
+
+class Calculator extends Component
+{
+    state ={};
+    render()
+    {
+        return(
+            <div>
+                <div className={"calculator"}>
+
+                   
+
+                </div>
+
+            </div>
+
+        );
+    }
+}
+export default Calculator;
