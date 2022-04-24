024f42795b785b7413db16e7d98743e98e3736fb
kfir9 <kfir950@gmail.com>
2022-04-24 12:43:04 +0300

    Basic Design

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ class App extends Component{
         <div className="App">
           <Suspense fallback={<div><Skeleton count={1}  height={100} />
               <Skeleton count={1}  height={100} baseColor={"#1D5C63"} />
-              <Skeleton count={1} height={400} />
+              <Skeleton circle width={60} height={60} baseColor={"black"} count={1} />
+              <Skeleton count={4} height={100} />
           </div>}>
               <Section bg={"white"} color={"black"} content={"BDS"}/>
               <Section bg={"#1D5C63"} color={"white"} content={<h1 style={{textAlign: "center"}} >Electric Cables Cross Section Calculation</h1>}/>
@@ -38,6 +39,7 @@ class App extends Component{
               <div className={"row"}>
                   <input className={"form-control"} style={{width: "100%"}} placeholder={"Remarks"} />
               </div>
+
               </div>
           </Suspense>
         </div>
