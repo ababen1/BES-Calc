7f1af7ddc97675db9524cac53b040f367ddd318a
kfir9 <kfir950@gmail.com>
2022-04-25 13:17:23 +0300

    Calculator design allmost done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css';
 const  Section = lazy(() =>{return new Promise(resolve => {setTimeout (()=> resolve(import("./components/comps/Section")),2500)});
 });
-
+const  Calculator = lazy(() =>{return new Promise(resolve => {setTimeout (()=> resolve(import("./components/comps/Calculator/Calculator")),2500)});
+});
 class App extends Component{
 
 
@@ -12,9 +13,9 @@ class App extends Component{
     return (
         <div className="App">
           <Suspense fallback={<div><Skeleton count={1}  height={100} />
-              <Skeleton count={1}  height={100} baseColor={"#1D5C63"} />
-              <Skeleton circle width={60} height={60} baseColor={"black"} count={1} />
-              <Skeleton count={4} height={100} />
+              <Skeleton count={1}  height={100} baseColor={"lightgray"} />
+              <Skeleton circle width={60} height={60} baseColor={"lightgray"} count={1} />
+              <Skeleton count={4} height={100} baseColor={"lightgray"} />
           </div>}>
               <Section bg={"white"} color={"black"} content={"BDS"}/>
               <Section bg={"#1D5C63"} color={"white"} content={<h1 style={{textAlign: "center"}} >Electric Cables Cross Section Calculation</h1>}/>
@@ -36,10 +37,16 @@ class App extends Component{
                       </div>
 
                   </div>
-              <div className={"row"}>
-                  <input className={"form-control"} style={{width: "100%"}} placeholder={"Remarks"} />
-              </div>
-
+                  <div className={"row"}>
+                      <div className={"col-12"}>
+                      <input className={"form-control"} style={{width: "100%"}} placeholder={"Remarks"} />
+                     </div>
+                 </div>
+                  <div className={"row"}>
+                      <div className={"col-12"}>
+                        <Calculator/>
+                      </div>
+                  </div>
               </div>
           </Suspense>
         </div>
--- src/components/comps/Calculator/Calculator.js
+++ src/components/comps/Calculator/Calculator.js
@@ -1,5 +1,5 @@
 import {Component} from "react";
-
+import "./calculator.css";
 class Calculator extends Component
 {
     state ={};
@@ -7,10 +7,46 @@ class Calculator extends Component
     {
         return(
             <div>
-                <div className={"calculator"}>
+                <div className={"calculator row"}>
+                    <div className={"add col-1"}>
+                        +
+                    </div>
+                    <div className={"description col-1"}>
+                        <input className={"form-control calculator-input"} placeholder={"Description"}  />
+                    </div>
+                    <div className={"kilo-w col-1"}>
+                        <select className={"KW"}>
+                            <option>power KW</option>
 
-                   
+                        </select>
+                    </div>
+                        <div className={"al-cu col-1"}>
+                            <label>AI</label>
+                            <label className="switch">
+                                <input type="checkbox" />
+                                    <span className="slider round"></span>
+                            </label>
+                            <label>Cu</label>
 
+                        </div>
+                    <div className={"al-cu col-1"}>
+                        <label>3 Wire</label>
+                        <label className="switch">
+                            <input type="checkbox" />
+                            <span className="slider round"></span>
+                        </label>
+                        <label>Single</label>
+
+                    </div>
+                    <div className={"data col-1"}>
+                       Reserve
+                    </div>
+                    <div className={"col-1 data"}>
+                        Imax
+                    </div>
+                    <div className={"col-1 data"}>
+                        s[mm^2]
+                    </div>
                 </div>
 
             </div>
