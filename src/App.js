5d6c0c65f8913523f2537f336635be7d8fbf5b07
kfir9 <kfir950@gmail.com>
2022-04-29 20:22:14 +0300

    Calculator design allmost done adding more functionality. Click functionality done.

--- src/App.js
+++ src/App.js
@@ -4,107 +4,18 @@ import LoginSignUp from "./components/comps/Modal/LoginSignUp";
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css';
 import  logo from "./Assets/Group 35.svg";
- import Section from "./components/comps/Section";
- import Calculator from "./components/comps/Calculator/Calculator";
-
+import Section from "./components/comps/Section";
+import Calculator from "./components/comps/Calculator/Calculator";
+import CalculatorPage from "./components/pages/CaculatorPage/CalculatorPage";
 class App extends Component{
-
-    state = {nowDate:new  Date(), calculatorRepeatNumber: 0,arrCalculator:[], arrCalculatorNumber:[]  ,focusInputOne:"normal", focusInputTwo: "normal", focusInputThree:"normal", ampacity:0}
-
-OnFocusBolderFont(ev)
-{
-    this.setState({name:"bold"});
-}
-
-OnFocusOut(){
-        this.setState({focus:"normal"});
-}
-
-//clicking on calculator adding to arrray in state
-OnClickCalculator()
-{
-    let arr = [];
-    let counter = this.state.calculatorRepeatNumber;
-    counter += 1;
-    this.setState({calculatorRepeatNumber: counter});
-    for(let i=0; i<=this.state.calculatorRepeatNumber; i++) {
-        arr.push(<Calculator count={i} />);
-    }
-    this.setState({arrCalculatorNumber: arr});
-
-}
-
-CalculatorNumber()
-{
-    let  counter = this.state.calculatorRepeatNumber;
-    for(let i=0; i<counter; i++)
-    {
-        this.state.arrCalculatorNumber.push(i)
-    }
-
-}
-
- //displaying calculators
-
-
-Calculation()
-{
-
-}
-
-  render(){
-
-    return (
-
-        <div className="App" >
-
-              <div className={"container"}>
-
-                      <Section bg={"white"} color={"black"} content={<img src={logo} className={"logo"} title={"BES"} alt={"BES"} />}/>
-                      <Section bg={"#156982"} color={"white"} content={<h1 style={{textAlign: "center"}} >Electric Cables Cross Section Calculation</h1>}/>
-
-                  <div className={"row"}>
-                      <div className={"col-12"}>
-
-                          <h3>{this.state.nowDate.toLocaleDateString()}</h3>
-
-                      </div>
-                  </div>
-
-                  <div className={"row"}>
-                      <div className={"col-6"}>
-                            <input name={"CustomerName"} onFocus={(e)=>{this.OnFocusBolderFont()}} onMouseOut={()=>{this.OnFocusOut()}} style={{fontWeight:this.state.focusInputOne}} className={"form-control"} placeholder={" Customer Name"} />
-                      </div>
-                      <div className={"col-6"}>
-                            <input name={"facilityName"} onFocus={this.OnFocusBolderFont.bind(this)} onMouseOut={this.OnFocusOut.bind(this)} style={{fontWeight:this.state.focusInputThree}}  className={"form-control"} placeholder={"Facility Name"} />
-                      </div>
-
-                  </div>
-                  <div className={"row"}>
-                      <div className={"col-12"}>
-                      <input name={"Remarks"} onFocus={this.OnFocusBolderFont.bind(this)} onMouseOut={this.OnFocusOut.bind(this)}  className={"form-control"} style={{width: "100%", fontWeight:this.state.focusInputThree}} placeholder={"Remarks"} />
-                     </div>
-                 </div>
-                  <div className={"row"}>
-                      <div className={"col-12"} onClick={this.OnClickCalculator.bind(this)}>
-                          {this.state.arrCalculatorNumber.map((value, index, array) => <Calculator key={index} count={index+1} />)}
-                          <Calculator count={0}/>
-                      </div>
-                  </div>
-                  <div className={"row"}>
-                      <div className={"col-12 space "}>
-
-                          <button className={"btn-secondary-disabled total-left"} type={"button"}>Calculate</button>
-                          <button className={"btn-secondary"} type={"button"}>Save</button>
-                      </div>
-
-                  </div>
-
-              </div>
+render() {
+    return(
+        <div>
+             <CalculatorPage />
         </div>
     );
+}
 
-  }
 
 }
 
--- src/components/comps/Calculator/Calculator.js
+++ src/components/comps/Calculator/Calculator.js
@@ -1,71 +1,82 @@
 import {Component} from "react";
 import "./calculator.css";
-class Calculator extends Component
-{
+class Calculator extends Component {
 
-    render()
-    {
-        return(
-            <div >
+    state = {description: ""};
 
-                <div   className={"calculator  row"}>
-                    <div style={{backgroundColor:(this.props.count===0)?"lightgray":"#156982"}}  className={"add col-1"}>
-                        {(this.props.count === 0)?"+":this.props.count}
-                    </div>
-                    <div className={"description col-1"}>
-                        <input className={"form-control calculator-input"} placeholder={"Description"}  />
-                        <hr className={"rule-line"} />
-                    </div>
+    OnChangeHandler(ev) {
+        this.setState({description: ev.target.value})
+    }
 
-                    <div className={"input kilo-w col-1"}>
-                        <select className={"KW"}>
-                            <option>power KW</option>
 
-                        </select>
-                        <hr className={"rule-line"}/>
 
-                    </div>
+    render() {
+            return (
+                <div>
+
+                    <div className={"calculator  row"}>
+                        <div style={{backgroundColor: (this.props.count === 0) ? "lightgray" : "#156982"}}
+                             className={"add col-1"}>
+                            {(this.props.count === 0) ? "+" : this.props.count}
+                        </div>
+                        <div className={"description col-1"}>
+                            <input name={"description"} onChange={this.OnChangeHandler.bind(this)}
+                                   className={"form-control calculator-input"} placeholder={"Description"}/>
+                            <hr className={"rule-line"}/>
+                        </div>
+
+                        <div className={"input kilo-w col-1"}>
+                            <select className={"KW"}>
+                                <option>power KW</option>
+
+                            </select>
+                            <hr className={"rule-line"}/>
+
+                        </div>
                         <div className={"input al-cu  col-1"}>
                             <label>AI</label>
                             <label className="switch">
-                                <input type="checkbox" />
-                                    <span className="slider round"></span>
+                                <input type="checkbox"/>
+                                <span className="slider round"></span>
                             </label>
                             <label>Cu</label>
                             <hr className={"rule-line"}/>
 
                         </div>
-                    <div className={"input al-cu col-1"}>
-                        <label>3 Wire</label>
-                        <label className="switch">
-                            <input type="checkbox" />
-                            <span className="slider round"></span>
-                        </label>
-                        <label>Single</label>
-                        <hr className={"rule-line-2"}/>
+                        <div className={"input al-cu col-1"}>
+                            <label>3 Wire</label>
+                            <label className="switch">
+                                <input type="checkbox"/>
+                                <span className="slider round"></span>
+                            </label>
+                            <label>Single</label>
+                            <hr className={"rule-line-2"}/>
 
-                    </div>
-                    <div className={"input data gray-text col-1"}>
-                      <input className={"form-control "} placeholder={"Reserve(%)"} />
-                        <hr className={"rule-line"}/>
+                        </div>
+                        <div className={"input data gray-text col-1"}>
+                            <input className={"form-control "} placeholder={"Reserve(%)"}/>
+                            <hr className={"rule-line"}/>
 
-                    </div>
-                    <div className={"input col-1 gray-text data"}>
-                      <input className={"form-control "} placeholder={"Imax"} />
-                        <hr className={"rule-line"}/>
+                        </div>
+                        <div className={"input col-1 gray-text data"}>
+                            <input className={"form-control "} placeholder={"Imax"}/>
+                            <hr className={"rule-line"}/>
 
-                    </div>
-                    <div className={"input col-1 gray-text data"}>
-                        <input className={"form-control "} placeholder={"S[Mm^2]"} />
-                        <hr className={"rule-line"}/>
+                        </div>
+                        <div className={"input col-1 gray-text data"}>
+                            <input className={"form-control "} placeholder={"S[Mm^2]"}/>
+                            <hr className={"rule-line"}/>
+
+                        </div>
 
                     </div>
 
                 </div>
 
-            </div>
+            );
 
-        );
+
     }
+
 }
 export default Calculator;
