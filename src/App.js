4e6ebee4b9dd937d763531a4b5e8cb861a35f787
ben <ben.egg.gov@gmail.com>
2022-07-20 23:25:10 +0300

    server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Component} from "react";
+import { Component } from "react";
 import 'react-loading-skeleton/dist/skeleton.css';
 import CalculatorPage from "./components/pages/CaculatorPage/CalculatorPage";
 import Container from 'react-bootstrap/Container'
@@ -7,16 +7,39 @@ import Header from './components/comps/Header';
 import logo from './Assets/Group 35.svg'
 import login_icon from "./Assets/login_icon.svg";
 
-class App extends Component{
-render() {
-    return(
-        <Container fluid className='App'>
-            <Header logo={logo} login_icon={login_icon}/>
-            <CalculatorPage />
+class App extends Component {
+    state = {
+        data: null,
+    };
 
-        </Container>
-    );
-}
+    componentDidMount() {
+        this.callBackendAPI()
+        .then((res) => {
+            this.setState({data: res.express});
+            console.log(this.state.data);
+        })
+        .catch((err) => console.log(err));
+    }
+
+    callBackendAPI = async () => {
+        const response = await fetch('/express_backend');
+        const body = await response.json();
+
+        if (response.status !== 200) {
+            throw Error(body.message);
+        }
+        return body;
+    }
+
+    render() {
+        return (
+            <Container fluid className='App'>
+                <Header logo={logo} login_icon={login_icon} />
+                <CalculatorPage />
+
+            </Container>
+        );
+    }
 
 
 }
