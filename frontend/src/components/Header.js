108f06f1e3684efead990c6e7384f165f09fab00
ben <ben.egg.gov@gmail.com>
2022-08-23 12:12:37 +0300

    header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,21 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { Stack, Image } from "react-bootstrap";
+import { Stack, Image, OverlayTrigger, Tooltip, Button } from "react-bootstrap";
 import LoginSignUp from "components/ModalLoginSignup/LoginSignUpPopup";
 import axios from "axios";
 import 'scss/HeaderFooter.scss'
 
 export default function Header(props) {
-    const [loginModalVisible, setLoginModalVisible] = useState(false);
+    const [showModal, setShowModal] = useState(false);
     const [username, setUsername] = useState("");
+    const [showTooltip, setShowTooltip] = useState(false);
 
-    const HandleUserIconClick = function(e) {
+    const HandleUserIconClick = function (e) {
         if (!username) {
-            setLoginModalVisible(true);
+            setShowModal(true);
         } else {
-            return // TODO
+            setShowTooltip(!showTooltip);
         }
     }
-    
+
     useEffect(() => {
         const configs = {
             method: "GET",
@@ -32,21 +33,33 @@ export default function Header(props) {
                 }
             })
             .catch((error) => { console.log(error) })
-    },[])
+    }, [])
 
     return (
         <div className="header">
-            <LoginSignUp visible={loginModalVisible} setVisible={setLoginModalVisible} />
+            <LoginSignUp visible={showModal} setVisible={setShowModal} />
             <div className="logo-and-login-section">
                 <Stack direction="horizontal">
                     <div className="BES-logo">
                         <Image src={props.logo} />
                     </div>
+
                     <div className="ms-auto login-icon">
-                        <Image src={props.login_icon} roundedCircle="true" width="50"
-                            onClick={HandleUserIconClick} /> <br />
+                        <OverlayTrigger
+                            placement={"bottom"}
+                            show={showTooltip}
+                            overlay={
+                                <Tooltip>
+                                    ababa
+                                </Tooltip>}>
+                            <Image src={props.login_icon} roundedCircle="true" width="50"
+                                onClick={HandleUserIconClick} />
+                        </OverlayTrigger>
+                        <br />
                         <span>{username != "" ? username : "Login"}</span>
                     </div>
+
+
                 </Stack>
             </div>
             <Stack className='electric-cables-title' >
