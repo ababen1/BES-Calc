5506d81652897167b9ec9df80facd44363ff4973
ben <ben.egg.gov@gmail.com>
2022-08-25 20:50:31 +0300

    changed the schema for saved calculation, and downloaded assets

--- models/Calculator.js
+++ models/Calculator.js
@@ -1,28 +1,28 @@
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 
-var calculatorSchema = new mongoose.Schema(
-    {
-        description: String,
-        powerKW: {
-            type: Number,
-            validate: {
-                validator: (value) => { return value in [15, 18.5, 22, 30, 37, 45, 55, 75, 90, 110, 132, 160, 200, 250, 280, 315, 355]},
-                message: "Invalid PowerKW"
-            },
-        },
-        cable: {
-            type: String,
-            enum: ['al', 'cu']
-        },
-        wire: {
-            type: String,
-            enum: ['single', '3wire']
-        },
-        ampacity: Number,
-        reserve: Number,
-        imax: Number,
-        smm2: Number, // TODO: find a better name for this 
-    }
-);
-calculatorSchema.path('powerKW').options.enum;
-mongoose.model("Calculator", calculatorSchema);
+// var calculatorSchema = new mongoose.Schema(
+//     {
+//         description: String,
+//         powerKW: {
+//             type: Number,
+//             validate: {
+//                 validator: (value) => { return value in [15, 18.5, 22, 30, 37, 45, 55, 75, 90, 110, 132, 160, 200, 250, 280, 315, 355]},
+//                 message: "Invalid PowerKW"
+//             },
+//         },
+//         cable: {
+//             type: String,
+//             enum: ['al', 'cu']
+//         },
+//         wire: {
+//             type: String,
+//             enum: ['single', '3wire']
+//         },
+//         ampacity: Number,
+//         reserve: Number,
+//         imax: Number,
+//         smm2: Number, // TODO: find a better name for this 
+//     }
+// );
+// calculatorSchema.path('powerKW').options.enum;
+// mongoose.model("Calculator", calculatorSchema);
--- models/SavedCalculation.js
+++ models/SavedCalculation.js
@@ -2,11 +2,12 @@ var mongoose = require('mongoose');
 
 var SavedCalculationSchema = new mongoose.Schema(
     {
-      user: { type: mongoose.Schema.Types.ObjectId, ref: 'user'},
+      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
       customer: String,
       facility: String,
       remakers: String,
-      calculators: [{type: mongoose.Schema.Types.ObjectId, ref: "Calculator"}],
+      date: Date,
+      calculations: mongoose.Schema.Types.Mixed
     });
 
     
